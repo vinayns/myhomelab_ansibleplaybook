ce5bdb3398d894bce516cb15cdfea7e81a124086
vinayns <davidbeckham752003@gmail.com>
2023-11-25 13:41:38 +0530

    Update installAnsible.sh

--- installAnsible.sh
+++ installAnsible.sh
@@ -1,4 +1,4 @@
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible -y
